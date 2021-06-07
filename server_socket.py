140b996f37e409a9e6869d970953515d2b727d1f
КаргаполовПавелАндре <SibehianBear86>
2021-06-07 23:43:09 +0500

    add port

--- server_socket.py
+++ server_socket.py
@@ -2,12 +2,15 @@ from bluetooth import *
 
 global connection
 
+port = 1024
+
+
 
 def initialise_server_socket():
     global connection
     server_socket = BluetoothSocket(RFCOMM)
 
-    server_socket.bind(('', 1024))
+    server_socket.bind(('', port))
 
     server_socket.listen(1)
     connection, address = server_socket.accept()
